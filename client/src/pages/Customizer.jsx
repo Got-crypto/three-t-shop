e3e0ac1be2863919c30a34a8d011b569686774c8
Got-crypto <pecpalmplay@gmail.com>
2024-02-09 04:45:56 +0200

    made customizer layout

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,6 +1,53 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+
+import { EditorTabs, FilterTabs } from "../config/constants"
+import state from '../store'
+
+import { fadeAnimtion, slideAnimation } from "../config/motion"
+
+import { CustomButton, Tab } from "../components"
 
 export default function Customizer() {
+    const snap = useSnapshot(state)
   return (
-    <div>Customizer</div>
+    <AnimatePresence>
+        {!snap.intro && (
+            <>
+                <motion.div
+                    key="custom"
+                    className="absolute top-0 left-0 z-10"
+                    {...slideAnimation('left')}
+                >
+                    <div className="flex items-center min-h-screen">
+                        <div className="editortabs-container tabs">
+                            {EditorTabs.map((tab) => (
+                                <Tab key={tab.name} tab={tab} handleCLick={() => {}} />
+                            ))}
+                        </div>
+                    </div>
+                </motion.div>
+
+                <motion.div className="absolute z-10 top-5 right-5" {...fadeAnimtion}>
+                    <CustomButton
+                        type={"filled"}
+                        title={"GO Back"}
+                        handleClick={() => snap.intro = true}
+                        customStyles={"w-fit px-4 py-2.5 font-bold tex-sm"}
+                    />
+                    
+                </motion.div>
+
+                <motion.div
+                    className="filterTabs-container"
+                    {...slideAnimation("up")}
+                >
+                    {FilterTabs.map((tab) => (
+                        <Tab key={tab.name} tab={tab} isFilterTab isActiveTab="" handleCLick={() => {}} />
+                    ))}
+                </motion.div>
+            </>
+        )}
+    </AnimatePresence>
   )
 }
