e0390e91f6fba97e393fb0c80b71d471cf373a0c
Got-crypto <pecpalmplay@gmail.com>
2024-02-14 02:10:46 +0200

    added a route to process prompt into image

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,12 +1,37 @@
 import * as dotenv from "dotenv";
 import express from "express";
+import OpenAI from "openai";
 
 dotenv.config()
 
 const router = express.Router()
 
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
 router.route('/').get((req, res) => {
     res.status(200).json({message: "Hello from DALL.E ROUTES"})
+})
+
+router.route("/").post(async (req, res) => {
+    try {
+        const {prompt} = req.body
+
+        const response = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: "bs4_json"
+        })
+
+        const photo = response.data.data[0].bs4_json
+
+        res.status(200).json({photo})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: "Something went wrong"})
+    }
 })
 
 export default router
